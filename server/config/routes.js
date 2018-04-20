af275226c36ffb792c19b3b485f177f5992e3d0b
sheetaldesai <sheetaldesai1@gmail.com>
2018-04-20 14:40:47 -0700

    course add/edit/delete

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,6 +6,14 @@ module.exports = function(app) {
         // get all courses
         course.getAllCourses(req, res)
     });
+    app.get('/courses/:id', function(req, res) {
+        // get all courses
+        course.getCourse(req, res)
+    });
+    app.put('/courses/:id', function(req, res) {
+        // edit course
+        course.editCourse(req, res)
+    });
     app.post('/course', function(req, res) {
         // Creates a new course.
         course.create(req, res)
@@ -16,8 +24,8 @@ module.exports = function(app) {
     // app.put('/polls/:id', function(req, res) {
     //     polls.updatePoll(req, res);
     // });
-    // app.delete('/polls/:id', function(req, res) {
-    //     polls.deletePoll(req, res);
-    // });
+    app.delete('/courses/:id', function(req, res) {
+        course.deleteCourse(req, res);
+    });
     app.all("*", (request, response) => { response.sendFile(path.resolve("../client/dist/index.html")) });
 }
--- server/controllers/course.js
+++ server/controllers/course.js
@@ -11,7 +11,7 @@ module.exports = {
             //console.log("Found questions: ", questions);
             res.json(questions);
         }).catch(function(err){
-            console.log("Error while getting questions: ",err);
+            console.log("Error while getting all courses: ",err);
             res.json(err);
         });
     },
@@ -26,5 +26,68 @@ module.exports = {
                 console.log("Error while creating new course", err);
                 res.json(err);
             }); 
+    },
+    getCourse: (req, res) => {
+        var id = req.params.id;
+        console.log("Get course id: ", id);
+        Course.find({_id:id}).then(function(course){
+            res.json(course);
+        }).catch(function(err){
+            console.log("Error while getting course with id ", id, err);
+            res.json(err);
+        });
+    },
+    editCourse: (req, res) => {
+        
+        var id = req.body._id;
+        console.log("edit course ", req.body);
+        // Course.findById(req.body._id, function(err, course){
+        //     console.log("found course: ", course);
+        //     course.title = req.body.title;
+        //     course.description = req.body.description;
+        //     course.startDaate = req.body.startDaate;
+        //     course.endDate = req.body.endDate;
+        //     course.meetingDays = req.meetingDays;
+        //     course.save(function(err) {
+        //         if (err) {
+        //             console.log("Error updaing course ", err);
+        //             res.json(err);
+        //         } else {
+        //             console.log("saved the course");
+        //             res.json('success');
+        //         }
+        //     });
+        // }).catch(function(err){
+        //     console.log("Error while getting course ",err);
+        //     res.json(err);
+       
+        Course.update({_id: id}, {
+            title : req.body.title,
+            description : req.body.description,
+            startDaate : req.body.startDaate,
+            endDate : req.body.endDate,
+            meetingDays : req.body.meetingDays
+        }, function(err, numberAffected, rawResponse){
+            if (err) {
+                console.log("Error updating course ", err)
+                res.json(err);
+            } else if (rawResponse) {
+                console.log("Updated course: ",rawResponse);
+                res.json('success');
+            }
+        })
+    },
+
+    deleteCourse: (req, res) => {
+        var id = req.params.id;
+        console.log("deleting course: ", id);
+        Course.remove({_id: id}, function(err, any){
+            if (err) {
+                res.json({status:'Error while removing course'});
+            } else {
+                console.log("deleted course");
+                res.json({status: 'Success'});
+            }
+        });
     }
 }
